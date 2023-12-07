b54cfdfa4e8d2ca24f403431402b8ce077d500c8
mkellner43 <matthew.kellner@outlook.com>
2023-12-07 09:21:49 -0600

    modified start script for building only

--- start.sh
+++ start.sh
@@ -11,7 +11,3 @@ export DJANGO_SUPERUSER_EMAIL="$DJANGO_SUPERUSER_EMAIL"
 export DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD"
 # Create a superuser from env variables
 python manage.py createsuperuser --noinput
-
-
-# Start the Django development server
-python manage.py runserver
