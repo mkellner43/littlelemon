67c325da97bc901f6eca97d5c94317b02aa8e989
mkellner43 <matthew.kellner@outlook.com>
2023-12-07 19:13:40 -0600

    prod

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
 # Run pip install to install dependencies
-pip install pipenv
-
-pipenv shell
-
-pipenv install
-
-python manage.py collectstatic --no-input
+pip install pipenv && pipenv shell && pipenv install && python manage.py collectstatic --no-input
