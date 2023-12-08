80692c8505a4f5563151527e28a1c8792fa258bc
mkellner43 <matthew.kellner@outlook.com>
2023-12-07 19:10:13 -0600

    prod

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -132,14 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # The settings for static files have been updated for the Graded assessment
-STATIC_URL = 'restaurant/static/'
-
-STATICFILES_DIRS = [
-    "restaurant/static",
-]
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
